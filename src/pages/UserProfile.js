0b957784b0662ac3378e3291e68249f4f0b81a86
Jim Grimes <jimgrimes86@gmail.com>
2023-10-12 15:27:25 -0400

    completed code-along

---
+++ src/pages/UserProfile.js
@@ -0,0 +1,21 @@
+import users from "../data";
+import NavBar from "../components/NavBar";
+import { useParams } from "react-router-dom";
+
+function UserProfile() {
+    const params = useParams();
+    const user = users.find(user => user.id === parseInt(params.id));
+
+    return (
+        <>
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <h1>{user.name}</h1>
+            </main>
+        </>
+    );
+};
+
+export default UserProfile;
